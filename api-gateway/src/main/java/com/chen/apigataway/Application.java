c41dd45cba8ffb9ff284c84c7aa0dc811d954aec
deyuanchen <321097355@qq.com>
2017-08-07 11:22:46 +0800

    api异常处理

--- api-gateway/src/main/java/com/chen/apigataway/Application.java
+++ api-gateway/src/main/java/com/chen/apigataway/Application.java
@@ -1,5 +1,7 @@
 package com.chen.apigataway;
 
+import com.netflix.zuul.FilterProcessor;
+import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -19,7 +21,13 @@ import org.springframework.context.annotation.Bean;
 public class Application {
 
     public static void main(String[] args) {
+        FilterProcessor.setProcessor(new DidiFilterProcessor());
         new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+    @Bean
+    public DefaultErrorAttributes errorAttributes() {
+        return new DidiErrorAttributes();
     }
 
     /**
