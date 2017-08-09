943f1f473d540fff6415c9a26ceb8d0ea7fcee88
deyuanchen <321097355@qq.com>
2017-08-09 17:27:18 +0800

    增加状态码的工具类

---
+++ chen-common/src/main/java/cn/chen/common/util/StatusCodeUtil.java
@@ -0,0 +1,12 @@
+package cn.chen.common.util;
+
+/**
+ * <p>Tiltle: cn.chen.common.util </p>
+ * <p>Description: 状态码信息工具 </p>
+ *
+ * @Author 陈德元
+ * @data: 2017-08-09
+ * @version: 1.0
+ */
+public class StatusCodeUtil {
+}
